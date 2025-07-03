88103b160408595f934b1c92e8892beb2a3b3c4f
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-03 21:10:56 +0600

    a new project installed with react router, tailwindcss, daisyui and also created browserrouter into the main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <button className="btn btn-primary">Success</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Root = () => {
+    const x =   5;
+function sayHi ( ){console.log('hi')}
+    return (
+        <div>
+            <h2>Hello from root</h2>
+        </div>
+    );
+};
+
+export default Root;
